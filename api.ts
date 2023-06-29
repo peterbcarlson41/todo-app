dc329d9f37637d97b10c8301dab1275b26a9efad
peterbcarlson41 <75817779+peterbcarlson41@users.noreply.github.com>
2023-06-29 15:21:12 -0400

    changed database location so I can deploy (hopefuly)

--- api.ts
+++ api.ts
@@ -1,6 +1,6 @@
 import { ITask } from "./types/tasks";
 
-const baseUrl = "http://localhost:3001";
+const baseUrl = "https://todo-list-data.onrender.com";
 
 export const getAllTodos = async (): Promise<ITask[]> => {
   const res = await fetch(`${baseUrl}/tasks`, { cache: "no-store" });
